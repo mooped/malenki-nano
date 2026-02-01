a24a4c6f874cc1fdbf86b5ba76527d6e6a543fe4
Steve Barnett <mooped@gmail.com>
2026-02-01 16:52:52 +0000

    6 channel of PWM using TCA0 in 16 bit mode via CMP0 interrupt TODO: implement downtime for 20ms period TODO: investigate radio timings and try to sync pulse outputs to end when we're not expecting interrupts from the radio Currently very WIP!

--- firmware/nano2/motors_pwm.c
+++ firmware/nano2/motors_pwm.c
@@ -10,19 +10,61 @@
 #define F_CPU 10000000 /* 10MHz */
 #include <util/delay.h>
 
-// Use the full cycle to reduce the time spent in interrupts
-#undef DUTY_MAX
-#define DUTY_MAX 255
+#define NUM_CHANNELS 7  // 6 channels as outputs and one for the downtime
 
-#define NUM_CHANNELS 6
+#define INT_MASK (TCA_SINGLE_CMP0_bm)
 
 // Desired PWM values for each output
-uint16_t pwm_values[NUM_CHANNELS] = { 0, 0, 0, 0, 0, 0 };
-uint16_t pwm_values_buffered[NUM_CHANNELS] = { 0, 0, 0, 0, 0, 0 };
+volatile uint16_t pwm_values[NUM_CHANNELS] = { 0, 0, 0, 0, 0, 0, 20000 };
+volatile uint16_t pwm_values_buffered[NUM_CHANNELS] = { 0, 0, 0, 0, 0, 0, 20000 };
 
-// Count TCA0 periods
-uint16_t period_count = 0;
+// Track which channel is being output
+volatile uint16_t active_channel = 0;
 
+// RX board has slightly wonky pin ordering to simplify layout
+//
+//   PA3 - Ch 6
+//   PA4 - Ch 1
+//   PA5 - Ch 2
+//
+//   PB0 - Ch 5
+//   PB1 - Ch 4
+//   PB2 - Ch 3
+
+static uint8_t dummy = 0; // Dummy register for downtime
+
+static volatile uint8_t* portclr_map[NUM_CHANNELS] =
+{
+    &(PORTA.OUTCLR),
+    &(PORTA.OUTCLR),
+    &(PORTB.OUTCLR),
+    &(PORTB.OUTCLR),
+    &(PORTB.OUTCLR),
+    &(PORTA.OUTCLR),
+    &(dummy),
+};
+
+static volatile uint8_t* portset_map[NUM_CHANNELS] =
+{
+    &(PORTA.OUTSET),
+    &(PORTA.OUTSET),
+    &(PORTB.OUTSET),
+    &(PORTB.OUTSET),
+    &(PORTB.OUTSET),
+    &(PORTA.OUTSET),
+    &(dummy),
+};
+
+static volatile uint8_t portmask_map[NUM_CHANNELS] =
+{
+    (1 << 4),
+    (1 << 5),
+    (1 << 2),
+    (1 << 1),
+    (1 << 0),
+    (1 << 3),
+    0,
+};
 static bool pin_check(char portname, PORT_t *port, uint8_t pin)
 {
     uint8_t bm = 1<<pin;
@@ -80,132 +122,20 @@ void motors_init()
     PORTA.DIRSET = 1 << 3;
     PORTA.DIRSET = 1 << 4;
     PORTA.DIRSET = 1 << 5;
-    //
-    // Timer A should be in split mode
-    //
-    // As CLK_PER is 3.33 mhz, 
-    // HOWEVER, in split mode we have 2x 8-bit timers,
-    // so we cannot count to 3000.
-    //
-    // We can count to 256 and tweak the compare values in interrupts
-    uint8_t period_val = DUTY_MAX;
-    // Enable split mode
-    TCA0.SPLIT.CTRLD = TCA_SPLIT_SPLITM_bm;
-    // Enable all comparators in split mode
-    TCA0.SPLIT.CTRLB = 
-        TCA_SPLIT_LCMP0EN_bm |
-        TCA_SPLIT_LCMP1EN_bm |
-        TCA_SPLIT_LCMP2EN_bm |
-        TCA_SPLIT_HCMP0EN_bm |
-        TCA_SPLIT_HCMP1EN_bm |
-        TCA_SPLIT_HCMP2EN_bm;
-    // Set the period - use the same period for both halves.
-    TCA0.SPLIT.HPER = period_val;
-    TCA0.SPLIT.LPER = period_val;
-    // Keep the halves in sync
-    TCA0.SPLIT.HCNT = 0;
-    TCA0.SPLIT.LCNT = 0;
-    // Finally, turn the timer on.
-    // Previously:
-    // Divide by 64, 3.333mhz / 64 =~ 52khz
-    // 52khz / period_val =~ 260hz = PWM frequency.
-    //
-    // Now:
-    // Divide by 2, 3.333mhz / 2 =~ 1.666mhz
-    // 1.666mhz / 256 =~ 6.5khz periods =~ 153.6 microsecond periods
-    // 8 periods is 1.228 milliseconds
-    // 
-    TCA0.SPLIT.CTRLA = 
-        //TCA_SPLIT_CLKSEL_DIV64_gc | // divide sys. clock by 64 
-        TCA_SPLIT_CLKSEL_DIV2_gc | // divide sys. clock by 2
-        TCA_SPLIT_ENABLE_bm;
-    // To set the PWM duty, we can now write to
-    // TCA0.HCMP0, .HCMP1, .HCMP2, .LCMP0, .LCMP1, .LCMP2
-    // these would have values of 0.. period_val
-    // here are sample values
-    // TCA0.SPLIT.LCMP0 = 80; // WO0 / PB0
-    // TCA0.SPLIT.LCMP1 = 100; // WO1 / PB1
-    // TCA0.SPLIT.LCMP2 = 120; // WO2 / PB2
-    // TCA0.SPLIT.HCMP0 = 20; // WO3 / pin PA3
-    // TCA0.SPLIT.HCMP1 = 40; // WO4 / PA4
-    // TCA0.SPLIT.HCMP2 = 60; // W05 / PA5
-    TCA0.SPLIT.HCMP0 = 0;
-    TCA0.SPLIT.LCMP0 = 0;
-    TCA0.SPLIT.LCMP2 = 0;
-    TCA0.SPLIT.LCMP1 = 0;
-    TCA0.SPLIT.HCMP2 = 0;
-    TCA0.SPLIT.HCMP1 = 0;
+
+    // Timer A in 16 bit mode
+    // Setting pins in motors_loop and clearing in CMP0 interrupt
+    uint16_t period_val = 20000;
+    uint16_t cmp_val = 0;
+    TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_SINGLESLOPE_gc;
+    TCA0.SINGLE.INTCTRL = INT_MASK;
+    TCA0.SINGLE.PERL = period_val & 0xff;
+    TCA0.SINGLE.PERH = (period_val & 0xff00) >> 8;
+    TCA0.SINGLE.CMP0L = (cmp_val & 0xff);
+    TCA0.SINGLE.CMP0H = (cmp_val & 0xff00) >> 8;
+    TCA0.SINGLE.INTFLAGS = 0;
+    TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV2_gc | TCA_SINGLE_ENABLE_bm;
 }
-
-// RX board has slightly wonky pin ordering to simplify layout
-//
-//   PA3 - Ch 6
-//   PA4 - Ch 1
-//   PA5 - Ch 2
-//
-//   PB0 - Ch 5
-//   PB1 - Ch 4
-//   PB0 - Ch 5
-
-// Pin IDs
-// Motor 1 = weapon
-#define MOTOR_1F 0
-#define MOTOR_1R 1
-// Left drive
-#define MOTOR_2F 2
-#define MOTOR_2R 3
-// Right drive
-#define MOTOR_3F 4
-#define MOTOR_3R 5
-
-// Table mapping the motor ID to the compare register
-// which controls its duty.
-// TODO: Update to reorder the channels
-static uint8_t volatile * motor_map[] = {
-    & (TCA0.SPLIT.HCMP0), // 1F, PA3
-    & (TCA0.SPLIT.LCMP0), // 1R, PB0
-    & (TCA0.SPLIT.LCMP2), // 2F, PB2
-    & (TCA0.SPLIT.LCMP1), // 2R, PB1
-    & (TCA0.SPLIT.HCMP2), // 3F, PA5
-    & (TCA0.SPLIT.HCMP1), // 3R, PA4
-};
-/*
-static uint8_t enable_map[] = {
-    TCA_SPLIT_HCMP0EN_bm, // 1F, PA3
-    TCA_SPLIT_LCMP0EN_bm, // 1R, PB0
-    TCA_SPLIT_LCMP2EN_bm, // 2F, PB2
-    TCA_SPLIT_LCMP1EN_bm, // 2R, PB1
-    TCA_SPLIT_HCMP2EN_bm, // 3F, PA5
-    TCA_SPLIT_HCMP1EN_bm, // 3R, PA4
-};
-*/
-static uint8_t volatile * invert_map[] = {
-    & (PORTA.PIN3CTRL), // 1F, PA3
-    & (PORTB.PIN0CTRL), // 1R, PB0
-    & (PORTB.PIN2CTRL), // 2F, PB2
-    & (PORTB.PIN1CTRL), // 2R, PB1
-    & (PORTA.PIN5CTRL), // 3F, PA5
-    & (PORTA.PIN4CTRL), // 3R, PA4
-};
-
-static void set_pin_duty(uint8_t signal_id, uint8_t duty)
-{
-    volatile uint8_t * compare_register=motor_map[signal_id]; 
-    volatile uint8_t * invert_register=invert_map[signal_id]; 
-    //uint8_t enable_mask=enable_map[signal_id];
-
-    if (duty == 255)
-    {
-      *invert_register &= ~PORT_INVEN_bm;
-      *compare_register = 0;
-    }
-    else
-    {
-      *invert_register |= PORT_INVEN_bm;
-      *compare_register = duty;
-    }
-}
-
 void set_motor_direction_duty(uint8_t motor_id, int16_t direction_and_duty)
 {
     // Not a thing on this board
@@ -218,95 +148,76 @@ void enable_motor_brake(uint8_t motor_id)
 
 void motors_loop()
 {
-
-}
-
-void motors_all_off()
-{
-    // Set all outputs to failsafe values
-    for (int channel_index = 0; channel_index < NUM_CHANNELS; ++channel_index)
+  // Once the interrupt is disabled, we can start the next pulse
+  if ((TCA0.SINGLE.INTCTRL & (INT_MASK)) != INT_MASK)
+  {
+    // Increment the channel counter
+    if (++active_channel >= 6)//NUM_CHANNELS)
     {
-      pwm_values[channel_index] = 1500;
+      active_channel = 0;
+
+      // If we're about to output channel 0, reload the latest set of values
+      for (int channel_index = 0; channel_index < NUM_CHANNELS; ++channel_index)
+      {
+        pwm_values[channel_index] = pwm_values_buffered[channel_index];
+      }
     }
 
-    set_pin_duty(0, 0);
-    set_pin_duty(1, 63);
-    set_pin_duty(2, 127);
-    set_pin_duty(3, 128);
-    set_pin_duty(4, 192);
-    set_pin_duty(5, 255);
-
-    // Enable the underflow interrupt for one channel (they should remain in sync)
-    //TCA0.SPLIT.INTCTRL =
-    //  TCA_SPLIT_LUNF_bm;
-}
-
-void set_pwm_outputs(uint16_t* sticks)
-{
-    /*
-    static uint8_t counter = 0;
-
-    if (counter != 0)
+    // Skip if the channel is set to 0 (failsafe)
+    if (pwm_values[active_channel] == 0)
     {
       return;
     }
 
-    ++counter;
+    cli();
+    // Update the timer for the next pulse
+    uint16_t cmp_val = pwm_values[active_channel] * 5;
+    TCA0.SINGLE.CMP0L = (cmp_val & 0xff);
+    TCA0.SINGLE.CMP0H = (cmp_val & 0xff00) >> 8;
 
-    // Buffer the new values for each channel
-    for (int channel_index = 0; channel_index < NUM_CHANNELS; ++channel_index)
-    {
-      pwm_values[channel_index] = sticks[channel_index];
-    }
+    // Reset the timer and set the selected pin high with minimum latency
+    TCA0.SINGLE.CNTL = 0;
+    *portset_map[active_channel] = portmask_map[active_channel];
+    TCA0.SINGLE.CNTH = 0;
 
-    // Enable the underflow interrupt for one channel (they should remain in sync)
-    TCA0.SPLIT.INTCTRL =
-      TCA_SPLIT_LUNF_bm;
-    */
-    set_pin_duty(0, 0);
-    set_pin_duty(1, 63);
-    set_pin_duty(2, 127);
-    set_pin_duty(3, 128);
-    set_pin_duty(4, 192);
-    set_pin_duty(5, 255);
+    // Re-enable timer interrupt
+    TCA0.SINGLE.INTCTRL = INT_MASK;
+    sei();
+  }
 }
 
-ISR(TCA0_LUNF_vect)
+void motors_all_off()
 {
-  ++period_count;
-
-  // Start the next cycle
-  if (period_count >= 130)
+  // Set all outputs to failsafe values - just flatline
+  for (int channel_index = 0; channel_index < NUM_CHANNELS; ++channel_index)
   {
-    // Disable the interrupt
-    //TCA0.SPLIT.INTCTRL =
-    //  TCA_SPLIT_LUNF_bm;
-
-    for (int channel_index = 0; channel_index < NUM_CHANNELS; ++channel_index)
-    {
-      pwm_values[channel_index] = 1500;//pwm_values_buffered[channel_index];
-    }
-    period_count = 0;
-  }
-  else
-  {
-    for (int channel_index = 0; channel_index < NUM_CHANNELS; ++channel_index)
-    {
-      uint16_t value = pwm_values[channel_index];
-
-      if (value == 0) // Expired
-      {
-        set_pin_duty(channel_index, 255);
-      }
-      else if (value < 256) // Expiring this cycle
-      {
-        set_pin_duty(channel_index, value);
-      }
-      else  // Expiring on a future cycle
-      {
-        set_pin_duty(channel_index, 128);
-        value -= 256;
-      }
-    }
+    pwm_values_buffered[channel_index] = 0;
   }
 }
+
+void set_pwm_outputs(uint16_t* sticks)
+{
+  // Buffer the new values for each channel
+  uint16_t sum = 0;
+  for (int channel_index = 0; channel_index < NUM_CHANNELS - 1; ++channel_index)
+  {
+    sum += sticks[channel_index];
+    pwm_values_buffered[channel_index] = sticks[channel_index];
+  }
+
+  // Final channel is the downtime i.e. 20ms minus all others
+  pwm_values_buffered[NUM_CHANNELS - 1] = 20000 - sum;
+}
+
+ISR(TCA0_CMP0_vect)
+{
+  // Clear interrupt flag
+  TCA0.SINGLE.INTFLAGS = TCA_SINGLE_CMP0_bm;
+
+  // Clear the current channel
+  *portclr_map[active_channel] = portmask_map[active_channel];
+
+  // Clear interrupt flag to signal motors_loop that we're ready for the next channel
+  TCA0.SINGLE.INTCTRL = 0x00;
+}
+
